3d1171e39155ebd4c7e59dd70e165c7b0fde9bb0
HKamilya <33655453+HKamilya@users.noreply.github.com>
2020-04-25 13:07:16 +0300

    profile with modal page

--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
      path("",views.login_0, name="login_0"),
      path("login",views.login_1, name="login_1"),
      path("profile",views.profile,name="profile"),
-     path("profile2",views.profile2,name="profile2"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -13,5 +13,4 @@ def profile(request):
     return render(request, "profile.html", {})
 
 
-def profile2(request):
-    return render(request, "profile2.html", {})
+
