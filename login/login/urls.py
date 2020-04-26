f0033b1b52b8bef1a828bb1ec717ab7193c83953
Dmitryitis <61817851+Dmitryitis@users.noreply.github.com>
2020-04-26 15:01:09 +0300

    Update urls.py

--- login/login/urls.py
+++ login/login/urls.py
@@ -5,7 +5,9 @@ from django.contrib.auth import views as auth_views
 from login import views
 
 urlpatterns = [
-    url(r'^login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    url(r'^login/', auth_views.LoginView.as_view(template_name='registration/login.html',
+                                                 authentication_form=LoginForm), name='login'),
     url(r'^profile/', views.ProfileView.as_view(), name='profile'),
     url(r'^logout/', auth_views.LogoutView.as_view(template_name='profile/profile.html', next_page=None), name='logout')
 ]
+
