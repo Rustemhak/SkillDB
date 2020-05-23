c658289a22fe578992dbfd3294f9327b2d3a1b62
Morozova Aleksandra <sashamorozova01@mail.ru>
2020-05-23 19:34:11 +0300

    events

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=120, default='Название мероприятия')
+    place = models.CharField(max_length=120, default='Место проведения')
+    date = models.CharField(max_length=120)
+    people = models.CharField(max_length=120, default='Количество людей')
+    donnates = models.TextField()
+    comments = models.TextField()
+    visible = models.BooleanField(default=1)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return "/%s/" % self.id
+
+    class Meta:
+        ordering = ["-id", "-timestamp"]
--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from django.urls import path, include
+from django.views.generic import DetailView
+
 from . import views
+from .models import Event
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url('event_info', views.main),
+    url(r'^$', views.main, name='main'),
+    url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Event, template_name = "main/event_info.html")),
 ]
